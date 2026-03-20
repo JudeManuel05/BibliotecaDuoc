68720cfaaafb973a3d02d6efc2c77158035d0ffc
Manuel <mane.perez@duocuc.cl>
2026-03-19 22:28:19 -0300

    actualizacion

--- src/main/java/com/example/biblotecaduoc/repository/LibroRepository.java
+++ src/main/java/com/example/biblotecaduoc/repository/LibroRepository.java
@@ -1,5 +1,78 @@
 package com.example.biblotecaduoc.repository;
 
-public class LibroRepositorio {
+
+import com.example.biblotecaduoc.BiblotecaduocApplication;
+import com.example.biblotecaduoc.model.Libro;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Repository
+
+public class LibroRepository {
+
+
+
+    private final BiblotecaduocApplication biblotecaduocApplication;
+    // Arreglo que guardara todos los libros
+    private List<Libro> listaLibros = new ArrayList<>();
+
+
+    LibroRepository(BiblotecaduocApplication biblotecaduocApplication) {
+        this.biblotecaduocApplication = biblotecaduocApplication;
+    }
+
+
+    // Metodo que retorna todos los libros 
+    public List<Libro> obtenLibros() {
+        return listaLibros;
+    }
+
+
+    // buscar libro por ID
+    public Libro buscarporId(int id){
+        for (Libro libro : listaLibros){
+            if (libro.getId() == id){
+                return libro;
+            }
+        }
+        return null;
+    }
+
+
+    // buscar Libro por su ISBN
+
+    public Libro buscarporIsbn(String isbn) {
+        for (Libro libro : listaLibros) {
+            if (libro.getIsbn().equals(isbn)) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    // Guardar Libro 
+    public Libro  guardar(Libro lib){
+        listaLibros.add(lib);
+        return lib;
+    }
+
+
+    // Actualizar Libro 
+    
+    public Libro actualizar(libro lib){
+        int id = 0;
+        int idPosicion = 0;
+        
+
+        for (int i = 0; i <ArrayLibros.size)();
+    }
+
+    
+
+
+
 
 }
